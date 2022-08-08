450e675f663740ecdb0d6b8a7f712d6879a861fe
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-08-08 15:32:35 +0800

    vite.config: add optional local config override

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
+import { existsSync, readFileSync } from 'fs'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+// Get working directory root
+const PROJECT_ROOT = dirname(fileURLToPath(import.meta.url))
+// Function to check for local config from local var
+function localConfigOptional(name) {
+	const path = resolve(PROJECT_ROOT, 'var', name)
+	if (existsSync(path)) {
+		const data = readFileSync(path).toString().trim() || undefined
+		console.log(`>> Using local config [${name}]: ${data}`)
+		try {
+			return JSON.parse(readFileSync(path))
+		} catch (e) {
+			return data
+		}
+	}
+}
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
@@ -10,18 +27,18 @@ export default defineConfig({
 		// eslint-disable-next-line spellcheck/spell-checker
 		dedupe: ['vue'],
 		alias: {
-			'@': path.resolve(__dirname),
-			'@C': path.resolve(__dirname, './common'),
-			'@CR': path.resolve(__dirname, './common/res'),
-			'@CL': path.resolve(__dirname, './common/lib'),
-			'@CS': path.resolve(__dirname, './common/store'),
-			'@CC': path.resolve(__dirname, './common/components'),
+			'@': resolve(__dirname),
+			'@C': resolve(__dirname, './common'),
+			'@CR': resolve(__dirname, './common/res'),
+			'@CL': resolve(__dirname, './common/lib'),
+			'@CS': resolve(__dirname, './common/store'),
+			'@CC': resolve(__dirname, './common/components'),
 		},
 	},
 	server: {
 		host: true,
 		hmr: {
-			// port: 443
+			port: localConfigOptional('HMR_PORT')
 		}
 	}
 })
